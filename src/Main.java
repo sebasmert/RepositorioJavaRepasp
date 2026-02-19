a49304c7f209f08d83f55d17d58efa3a06b54552
SebasM <sebasmert.071203@gmail.com>
2026-02-19 12:42:26 -0500

    datos del programador

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Sebastian Alejandro Merchan Torres");
+        System.out.println("Zipaquira");
+        System.out.println("EDAD: 23");
+        System.out.println("Kiara");
     }
 }
